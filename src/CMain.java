90d65f1d9bf189bc1b14cad16ee6601a848a7bcc
Boostvolt <51777660+Boostvolt@users.noreply.github.com>
2021-03-14 10:46:13 +0100

    Updated names

--- src/CMain.java
+++ src/CMain.java
@@ -9,9 +9,9 @@ public class CMain {
 
 	/* Folgende Argumente zur JVM hinzufügen: -Xdock:name="Einarmiger Bandit" -Xdock:icon=images/icon.png -Dapple.awt.application.appearance=system */
 	public static void main(String[] args) {
-		CView view = new CView();
-		CModel model = new CModel();
-		CController controller = new CController(view, model);
-		controller.initializeController();
+		CView myView = new CView();
+		CModel myModel = new CModel();
+		CController myController = new CController(myView, myModel);
+		myController.initializeController();
 	}
 }
